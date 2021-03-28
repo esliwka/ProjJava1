6402e3ab8b90a57988b17c36d452298628a23c30
esliwka <e_sliwka@tuta.io>
2021-03-28 17:27:53 +0200

    Added: Solid base class Ball class

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,5 @@
 public class MainClass {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hewwo");
     }
 }
---
+++ src/Solid.java
@@ -0,0 +1,12 @@
+public class Solid {
+    private String type;
+
+    public Solid(String thing) {
+        super();
+        this.type = thing;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
