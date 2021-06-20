c4403912459aeae0de586b27a511fffbec18dae3
esliwka <e_sliwka@tuta.io>
2021-06-20 14:56:24 +0200

    Changed file handling from scanner to filestream

--- src/DrawMainFrame.java
+++ src/DrawMainFrame.java
@@ -2,10 +2,8 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.event.ItemEvent;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.Scanner;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
@@ -53,7 +51,7 @@ public class DrawMainFrame extends JFrame implements Runnable {
         add(buttonPanel, BorderLayout.SOUTH);
         pack();
     }
-    
+
     // panel with input elements
     class CalculationPanel extends JPanel {
 
@@ -61,7 +59,7 @@ public class DrawMainFrame extends JFrame implements Runnable {
 
         public CalculationPanel(JFrame parentFrame) {
             this.parentFrame = parentFrame;
-            setPreferredSize(new Dimension(parentFrame.getWidth(), parentFrame.getHeight()/5));
+            setPreferredSize(new Dimension(parentFrame.getWidth(), parentFrame.getHeight() / 5));
             DefaultComboBoxModel<String> dcbm = new DefaultComboBoxModel<>(solidList);
             cbSolidList = new JComboBox<String>(dcbm);
             add(cbSolidList);
@@ -77,75 +75,75 @@ public class DrawMainFrame extends JFrame implements Runnable {
             add(c);
 
             cbSolidList.addItemListener(
-                e -> {
+                    e -> {
 
-                    if(e.getStateChange() == ItemEvent.SELECTED) {
+                        if (e.getStateChange() == ItemEvent.SELECTED) {
 
-                        switch (e.getItem().toString()) {
-                            case "Cuboid" -> {
-                                a.setVisible(true);
-                                aLabel.setText("a = ");
-                                b.setVisible(true);
-                                bLabel.setText("b = ");
-                                c.setVisible(true);
-                                cLabel.setText("h = ");
-                                s1 = new Cuboid();
-                            }
+                            switch (e.getItem().toString()) {
+                                case "Cuboid" -> {
+                                    a.setVisible(true);
+                                    aLabel.setText("a = ");
+                                    b.setVisible(true);
+                                    bLabel.setText("b = ");
+                                    c.setVisible(true);
+                                    cLabel.setText("h = ");
+                                    s1 = new Cuboid();
+                                }
 
-                            case "Cube" -> {
-                                a.setVisible(true);
-                                aLabel.setText("a = ");
-                                b.setVisible(false);
-                                bLabel.setText("");
-                                c.setVisible(false);
-                                cLabel.setText("");
-                                s1 = new Cube();
-                            }
+                                case "Cube" -> {
+                                    a.setVisible(true);
+                                    aLabel.setText("a = ");
+                                    b.setVisible(false);
+                                    bLabel.setText("");
+                                    c.setVisible(false);
+                                    cLabel.setText("");
+                                    s1 = new Cube();
+                                }
 
-                            case "Cone" -> {
-                                a.setVisible(true);
-                                aLabel.setText("r = ");
-                                b.setVisible(true);
-                                bLabel.setText("h = ");
-                                c.setVisible(true);
-                                cLabel.setText("l = ");
-                                s1 = new Cone();
-                            }
+                                case "Cone" -> {
+                                    a.setVisible(true);
+                                    aLabel.setText("r = ");
+                                    b.setVisible(true);
+                                    bLabel.setText("h = ");
+                                    c.setVisible(true);
+                                    cLabel.setText("l = ");
+                                    s1 = new Cone();
+                                }
 
-                            case "Cylinder" -> {
-                                a.setVisible(true);
-                                aLabel.setText("r = ");
-                                b.setVisible(true);
-                                bLabel.setText("h = ");
-                                c.setVisible(false);
-                                cLabel.setText("");
-                                s1 = new Cylinder();
-                            }
+                                case "Cylinder" -> {
+                                    a.setVisible(true);
+                                    aLabel.setText("r = ");
+                                    b.setVisible(true);
+                                    bLabel.setText("h = ");
+                                    c.setVisible(false);
+                                    cLabel.setText("");
+                                    s1 = new Cylinder();
+                                }
 
-                            case "Sphere" -> {
-                                a.setVisible(true);
-                                aLabel.setText("r = ");
-                                b.setVisible(false);
-                                bLabel.setText("");
-                                c.setVisible(false);
-                                cLabel.setText("");
-                                s1 = new Sphere();
+                                case "Sphere" -> {
+                                    a.setVisible(true);
+                                    aLabel.setText("r = ");
+                                    b.setVisible(false);
+                                    bLabel.setText("");
+                                    c.setVisible(false);
+                                    cLabel.setText("");
+                                    s1 = new Sphere();
+                                }
                             }
                         }
-                    }
-                });
+                    });
 
         }
     }
-    
+
     // panel displaying results
     class ResultPanel extends JPanel {
-        
+
         private JFrame parentFrame;
 
         public ResultPanel(JFrame parentFrame) {
             this.parentFrame = parentFrame;
-            setPreferredSize(new Dimension(parentFrame.getWidth(), parentFrame.getHeight()/10*7));
+            setPreferredSize(new Dimension(parentFrame.getWidth(), parentFrame.getHeight() / 10 * 7));
 
             result = new JLabel("");
             add(result);
@@ -160,104 +158,100 @@ public class DrawMainFrame extends JFrame implements Runnable {
 
         public ButtonPanel(JFrame parentFrame) {
             this.parentFrame = parentFrame;
-            setPreferredSize(new Dimension(parentFrame.getWidth(), parentFrame.getHeight()/10));
+            setPreferredSize(new Dimension(parentFrame.getWidth(), parentFrame.getHeight() / 10));
 
             // adding buttons
             // file button - process input from file like in project 1
             fileButton = new JButton("File");
             fileButton.addActionListener(
-                e -> {
-                    result.setText("");
-                    // choose a file with input like in project 1 and process it, output to file
-                    JFileChooser fc = new JFileChooser(new File("."));
-                    fc.setMultiSelectionEnabled(false);
-                    fc.setFileFilter(new FileFilter(){
+                    e -> {
+                        result.setText("");
+                        // choose a file with input like in project 1 and process it, output to file
+                        JFileChooser fc = new JFileChooser(new File("."));
+                        fc.setMultiSelectionEnabled(false);
+                        fc.setFileFilter(new FileFilter() {
 
-                        @Override
-                        public boolean accept(File f) {
-                            return f.getName().contains("dat") || f.getName().contains("txt");
-                        }
-
-                        @Override
-                        public String getDescription() {
-                            // TODO Auto-generated method stub
-                            return null;
-                        }
-                        
-                    });
-                    
-                    int result = fc.showOpenDialog(parentFrame);
-                    if(result == JFileChooser.APPROVE_OPTION) {
-
-                        // ############################################################
-                        // reading input filepath of the file pointed to by the user
-                        String sfilePath = fc.getSelectedFile().getAbsolutePath();
-                        // input output files
-                        File fin;
-                        if (sfilePath == null || sfilePath.isEmpty())
-                            fin = new File("input.dat");
-                        else
-                            fin = new File(sfilePath);
-                        File fout = new File("output.dat");
-                        // reading input file, executing calculations, writing output
-                        try (Scanner sc = new Scanner(fin)) {
-                            PrintWriter pw = new PrintWriter(fout);
-                            try {
-                                while (sc.hasNextLine()) {
-                                    FileHandling fh = new FileHandling();
-                                    s1 = fh.parseLine(sc);
-                                    System.out.println(s1.toString());
-                                    // writing to the output file
-                                    fh.saveToFile(s1.toString(), pw);
-                                }
-                            } finally {
-                                DrawMainFrame.this.result.setText("Results saved to file \"output.dat\"");
-                                sc.close();
+                            @Override
+                            public boolean accept(File f) {
+                                return f.getName().contains("dat") || f.getName().contains("txt");
                             }
+
+                            @Override
+                            public String getDescription() {
+                                // TODO Auto-generated method stub
+                                return null;
+                            }
+
+                        });
+
+                        int result = fc.showOpenDialog(parentFrame);
+                        if (result == JFileChooser.APPROVE_OPTION) {
+
+                            // ############################################################
+                            // reading input filepath of the file pointed to by the user
+                            String sfilePath = fc.getSelectedFile().getAbsolutePath();
+                            // input output files
+                            File fin;
+                            if (sfilePath == null || sfilePath.isEmpty())
+                                fin = new File("input.dat");
+                            else
+                                fin = new File(sfilePath);
+                            File fout = new File("output.dat");
+                            // reading input file, executing calculations, writing output
+                            try (final var is = new FileInputStream(fin)) {
+                                PrintWriter pw = new PrintWriter(fout);
+                                final var r = new InputStreamReader(is, StandardCharsets.UTF_8);
+                                final var br = new BufferedReader(r);
+                                FileHandling fh = new FileHandling();
+                                br.lines().map(fh::parseLine)
+                                        .forEach(solid -> {
+                                            fh.saveToFile(solid.toString(), pw);
+                                            System.out.println(solid.toString());
+                                        });
+                                DrawMainFrame.this.result.setText("Output saved to output.dat");
+                            }
+                            // catching exceptions
+                            catch (FileNotFoundException ex) {
+                                System.out.println("File not found");
+                                DrawMainFrame.this.result.setText("File not found");
+                            } catch (ZeroException ex) {
+                                System.out.println(ex.getMessage());
+                                DrawMainFrame.this.result.setText(ex.getMessage());
+                            } catch (NegativeException ex) {
+                                System.out.println(ex.getMessage());
+                                DrawMainFrame.this.result.setText(ex.getMessage());
+                            } catch (IllegalStateException ex) {
+                                System.out.println("Solid type not recognized: " + ex.getMessage());
+                                DrawMainFrame.this.result.setText("Solid type not recognized: " + ex.getMessage());
+                            } catch (IOException ioException) {
+                                ioException.printStackTrace();
+                            }
+                            // ############################################################
                         }
-                        // catching exceptions
-                        catch(FileNotFoundException ex) {
-                            System.out.println("File not found");
-                            DrawMainFrame.this.result.setText("File not found");
-                        }
-                        catch(ZeroException ex) {
-                            System.out.println(ex.getMessage());
-                            DrawMainFrame.this.result.setText(ex.getMessage());
-                        }
-                        catch(NegativeException ex) {
-                            System.out.println(ex.getMessage());
-                            DrawMainFrame.this.result.setText(ex.getMessage());
-                        }
-                        catch(IllegalStateException ex) {
-                            System.out.println("Solid type not recognized: " + ex.getMessage());
-                            DrawMainFrame.this.result.setText("Solid type not recognized: " + ex.getMessage());
-                        }
-                        // ############################################################
-                    }
-                });
+                    });
             add(fileButton);
 
             // clear button
             clearButton = new JButton("Clear all");
             clearButton.addActionListener(
-                e -> {
-                    // clear input fields and result panel
-                    a.setText("");
-                    b.setText("");
-                    c.setText("");
-                    result.setText("");
-                });
+                    e -> {
+                        // clear input fields and result panel
+                        a.setText("");
+                        b.setText("");
+                        c.setText("");
+                        result.setText("");
+                    });
             add(clearButton);
 
             // calculate button
             calculateButton = new JButton("Calculate");
             calculateButton.addActionListener(
-                e -> {
-                    // calculate and display results in the results panel
-                    InputParsing ip = new InputParsing();
-                    ip.screenCalculate(s1, a, b, c, result);
-                    
-                });
+                    e -> {
+                        // calculate and display results in the results panel
+                        InputParsing ip = new InputParsing();
+                        ip.screenCalculate(s1, a, b, c, result);
+
+                    });
             add(calculateButton);
         }
 
--- src/FileHandling.java
+++ src/FileHandling.java
@@ -1,17 +1,16 @@
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class FileHandling {
     
     // parsing line from input file depending on the type of solid
-    public Solid parseLine(Scanner sc) throws ZeroException, NegativeException {
-
-        String solidType = sc.next();
-        String line = sc.nextLine();
+    public Solid parseLine(String line) {
         StringTokenizer st = new StringTokenizer(line);
-        Solid s1 = switch (solidType) {
+        String solidType = st.nextToken();
+        return switch (solidType) {
             case "Cone" ->
             {
                 Double t1,t2,t3;
@@ -55,24 +54,23 @@ public class FileHandling {
             }
             default -> throw new IllegalStateException(solidType);
         };
-        return s1;
     }
 
     // writing to output file
-    public void saveToFile(String str, PrintWriter pw) throws FileNotFoundException{
+    public void saveToFile(String str, PrintWriter pw) {
         pw.println(str);
         pw.flush();
     }
 
 }
 
-class ZeroException extends Exception {
+class ZeroException extends RuntimeException {
     public ZeroException(){
         super("Input can't be 0");
     }
 }
 
-class NegativeException extends Exception {
+class NegativeException extends RuntimeException {
     public NegativeException(){
         super("Input can't be negative");
     }
